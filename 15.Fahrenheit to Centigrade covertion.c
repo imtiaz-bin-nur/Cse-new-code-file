247ce41a234dbceefd5a83cd574fcb9870d1715f
imtiaz-bin-nur <imtiazmr798@gmail.com>
2025-03-21 17:59:29 +0600

    Create 15.Fahrenheit to Centigrade covertion.c

---
+++ 15.Fahrenheit to Centigrade covertion.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main( )
+{
+float Fahrenheit,celcious;
+printf ("Enterter temperature in Fahrenheit: ");
+scanf ("f",&Fahrenheit);
+Celsius =(farenhite_32)×1.8;
+printf ("\temperature in Celsius:%2f",Celsius);
+return 0;
+}
