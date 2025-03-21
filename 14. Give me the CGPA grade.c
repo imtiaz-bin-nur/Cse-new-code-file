168bdd8e320247ece7a99feac074d566c5f6cd70
imtiaz-bin-nur <imtiazmr798@gmail.com>
2025-03-21 17:56:59 +0600

    Create 14. Give me the CGPA grade.c

---
+++ 14. Give me the CGPA grade.c
@@ -0,0 +1,27 @@
+#include <stdio.h >
+int main ()
+{
+int number =86
+if (number >=80)
+{
+printf ("A+");
+}
+else if(number >=70)
+{
+printf ("A");
+}
+else if (number >=60)
+{
+printf ("A_");
+}
+else if (number >=50)
+{
+printf ("B");
+}
+else if (number >=40);
+{
+printf ("C");
+}
+else printf ("F");
+return 0;
+}
