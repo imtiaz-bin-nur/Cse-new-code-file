7f08047772d1ae6879911f45f19ceb4cef329927
imtiaz-bin-nur <imtiazmr798@gmail.com>
2025-03-21 16:20:40 +0600

    Create 1.Two number add sub mul div.c

---
+++ 1.Two number add sub mul div.c
@@ -0,0 +1,14 @@
+#include <stdio.h >
+int main ()
+{
+  float a,b,add,sub,mul,div;
+printf ("Enter a number ");
+scanf ("%f",&a);
+printf ("Enter another number);
+  scanf ("%f",&b);
+add=a+b;
+sub=a_b;
+mul=a*b;
+div=a÷b
+printf ("addition =%2f\n subtraction =%2f\n multiplication =%2f\n division =%2f,add,sub,mul,div);
+}
