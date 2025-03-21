678dbe7b7ca9bb1bd54927669556c5028f2581d0
imtiaz-bin-nur <imtiazmr798@gmail.com>
2025-03-21 17:52:26 +0600

    Create 12.Find squre root of a number.c

---
+++ 12.Find squre root of a number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+#include < math.h>
+void main()
+{
+ int a squareroot;
+ printf ("Enter any number: ");
+ scanf ("%d", &a);
+squreroot=sqrt(a);
+printf ("squreroot is :%d",squreroot);
+return 0;
+}
