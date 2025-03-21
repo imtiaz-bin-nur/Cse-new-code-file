1f72cfa82069b3f607d6083db465543c74a5e842
imtiaz-bin-nur <imtiazmr798@gmail.com>
2025-03-21 17:41:29 +0600

    Create 8.Find the greatest number among the 3 number.c

---
+++ 8.Find the greatest number among the 3 number.c
@@ -0,0 +1,20 @@
+#include <stdio.h >
+int main  ()
+{
+int a , b , c , big ;
+printf  ("Enter numbers\n");
+scanf("d %d %d", &a &b &c);
+If(a>=b)
+{
+ If(a>=c)
+printf ("%d is biggest\n" ,a);
+else 
+printf ("%d is the largest number \n",c);
+}
+else
+{
+ If(b>=c)
+printf ("%d is the largest number \n",b);
+else printf ("%d"% , is the largest number \",c);
+return 0 ;
+}
