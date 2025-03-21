06cfc3f0d64d6eb5956345ef0cc7d4e437bd1fcd
imtiaz-bin-nur <imtiazmr798@gmail.com>
2025-03-21 17:43:39 +0600

    Create 9.Find the smallest number among number.c

---
+++ 9.Find the smallest number among number.c
@@ -0,0 +1,19 @@
+#include <stdio.h >
+int main ()
+{
+int a,b,c;
+printf ("Enter 3 no."s");
+If(a<b && a<c);
+{ 
+printf ("%d is smallest number ", a);
+}
+else if (b<c)
+{
+printf (%d is smallest number ",b);
+}
+else
+{ 
+ printf ("%d is smallest number ",c);
+}
+return 0;
+}
